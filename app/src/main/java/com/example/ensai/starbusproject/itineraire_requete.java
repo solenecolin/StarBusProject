d47c287d13dd3cf8732fd92f2f974e38562795f7
loicLaporte <loic.laporte@eleve.ensai.fr>
2016-05-10 15:12:49 +0200

    g

--- app/src/main/java/com/example/ensai/starbusproject/itineraire_requete.java
+++ app/src/main/java/com/example/ensai/starbusproject/itineraire_requete.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.AutoCompleteTextView;
+
+import static com.example.ensai.starbusproject.R.id.itineraire_depart;
 
 public class itineraire_requete extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,8 +19,20 @@ public class itineraire_requete extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itineraire_requete);
         bou_res = (Button) findViewById(R.id.bou_itineraire_chercher);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(itineraire_depart);
+        textView.setAdapter(adapter);
         bou_res.setOnClickListener(this);
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+
+
 
     public void onClick(View v) {
         if (v.getId() == R.id.bou_itineraire_chercher) {
