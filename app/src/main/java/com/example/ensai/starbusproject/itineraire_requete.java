e468b62e382be240b65edf7781a3e5caf3a2cfe7
loicLaporte <loic.laporte@eleve.ensai.fr>
2016-05-10 12:46:33 +0200

    C'est un gars, il rentre dans un bar, et il dit "Ouille".

---
+++ app/src/main/java/com/example/ensai/starbusproject/itineraire_requete.java
@@ -0,0 +1,24 @@
+package com.example.ensai.starbusproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class itineraire_requete extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itineraire_requete);
+        Button button = (Button) findViewById(R.id.itineraire_chercher);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, itineraire_resultat.class);
+        startActivity(intent);
+    }
+}
