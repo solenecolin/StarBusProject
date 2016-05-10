ccdaf80fe58b096b789a2e15b4cd2b6792b8be0a
loicLaporte <loic.laporte@eleve.ensai.fr>
2016-05-10 16:30:28 +0200

    route

--- app/src/main/java/com/example/ensai/starbusproject/Route.java
+++ app/src/main/java/com/example/ensai/starbusproject/Route.java
@@ -3,5 +3,36 @@ package com.example.ensai.starbusproject;
 /**
  * Created by ensai on 10/05/16.
  */
+
 public class Route {
+    public String route_short_name;
+    public String route_long_name;
+
+
+    public Route(){
+
+    }
+
+
+
+    public String getRoute_short_name(String route_short_name){
+        return route_short_name;
+    }
+
+    public String setRoute_short_name(String route_short_name){
+        return route_short_name;
+    }
+
+
+
+
+
+    public String getRoute_long_name(String route_long_name){
+        return route_long_name;
+    }
+
+    public String setRoute_long_name(String route_long_name){
+        return route_long_name;
+    }
+
 }
