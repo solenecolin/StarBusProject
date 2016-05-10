2cc0c6f7665469578219a4fc7e9b5e9f531b40ee
solenecolin <soso.c93@hotmail.fr>
2016-05-10 14:15:42 +0200

    test

--- app/src/main/java/com/example/ensai/starbusproject/Main.java
+++ app/src/main/java/com/example/ensai/starbusproject/Main.java
@@ -14,18 +14,21 @@ import android.widget.Toast;
 public class Main extends AppCompatActivity implements View.OnClickListener {
 
     Button bou_hor = null;
+    Button bou_iti = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bou_hor = (Button) findViewById(R.id.bou_hor);
-        bou_hor.setOnClickListener(new View.OnClickListener());
+        bou_hor.setOnClickListener(this);
+        bou_iti = (Button) findViewById(R.id.bou_hor);
+        bou_iti.setOnClickListener(this);
     }
 
     public void onClick(View v){
         if (v.getId()==R.id.bou_iti){
-            Intent i = new Intent(this, Itineraire_requete.class);
+            Intent i = new Intent(this, itineraire_requete.class);
             startActivity(i);
         }
         if (v.getId()==R.id.bou_hor){
