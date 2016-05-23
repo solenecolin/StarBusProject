405c39a472a731c8b5c84bd109126675a8692bc0
solenecolin <soso.c93@hotmail.fr>
2016-05-23 22:26:47 +0200

    c'est un peu le dawa pour horaire...

---
+++ app/src/main/java/com/example/ensai/starbusproject/ArretAdapter.java
@@ -0,0 +1,65 @@
+package com.example.ensai.starbusproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class ArretAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Arret> arrets;
+
+    public ArretAdapter(Context context,List<Arret> arrets) {
+        this.arrets = arrets;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return arrets.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return arrets.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+        if (convertView == null) {
+            if ( convertView == null ) {
+                view = ( (LayoutInflater)
+                        context . getSystemService ( Context . LAYOUT_INFLATER_SERVICE ) ) . inflate ( R.layout.layoutarret, parent , false ) ;
+            }
+        }
+        else {
+            view = convertView;
+        }
+
+        Arret arret = (Arret) getItem(position);
+        TextView nom = (TextView) view.findViewById(R.id.nom);
+        TextView passage = (TextView) view.findViewById(R.id.passage);
+        nom.setText(arret.getStop());
+        passage.setText((arret.getDepartures().size() == 0 ? "Pas de passage" : arret.getDepartures().get(0).getContent().toString()));
+        return view;
+    }
+
+
+
+
+
+}
